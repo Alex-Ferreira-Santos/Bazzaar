0c92533c1babd20d10cea4fcb7bb77d4f21c8ebc
Alex <64551315+Alex-Ferreira-Santos@users.noreply.github.com>
2021-02-03 09:56:26 -0300

    class header

---
+++ src/styles/index.js
@@ -0,0 +1,17 @@
+import {StyleSheet} from 'react-native'
+
+const styles = StyleSheet.create({
+    container:{
+        flex: 1,
+    },
+    contentContainer:{
+        flex:1,
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+    title:{
+        fontSize: 50
+    }
+});
+
+export default styles
