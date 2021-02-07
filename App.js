258359f5d57bbaf817ddf537e87a4c2eabe142f1
Alex <64551315+Alex-Ferreira-Santos@users.noreply.github.com>
2021-02-07 11:03:24 -0300

    stack com tab navigation

--- App.js
+++ App.js
@@ -1,6 +1,7 @@
 import React,{Component} from 'react';
 import {NavigationContainer} from '@react-navigation/native'
 import {createBottomTabNavigator} from '@react-navigation/bottom-tabs'
+import {createStackNavigator} from '@react-navigation/stack'
 
 import Home from './src/components/HomePage'
 import Categorias from './src/components/Categorias'
@@ -8,8 +9,10 @@ import Categorias from './src/components/Categorias'
 import Icon from 'react-native-vector-icons/Ionicons'
 import Sobre from './src/components/Sobre';
 import Cartoes from './src/components/Cartoes';
+import Produto from './src/components/Produto'
 
 const Tab = createBottomTabNavigator()
+const Stack = createStackNavigator()
 const icons = {
   Home:{
     name:'home'
@@ -24,9 +27,17 @@ const icons = {
     name:'card-outline'
   }
 }
+
+function ProdutoNav(){
+  return(
+    <Stack.Navigator>
+      <Stack.Screen name='Categorias' component={Categorias} initialParams={{promocoes: false, show: true, text: 'Ocultar', name: 'arrow-up-sharp',feminino: false,masculino:false,novidades: false}} options={{ headerShown: false }}/>
+      <Stack.Screen name='Produto' component={Produto} options={{ headerShown: false }}/>
+    </Stack.Navigator>
+  )
+}
+
 class App extends Component {
-
-
   render(){
     return (
       <NavigationContainer>
@@ -37,10 +48,10 @@ class App extends Component {
           }
         })}>
           <Tab.Screen name='Home' component={Home}/>
-          <Tab.Screen name='Categorias' component={Categorias} initialParams={{promocoes: false, show: true, text: 'Ocultar', name: 'arrow-up-sharp',feminino: false,masculino:false,novidades: false}}/>
+          <Tab.Screen name='Categorias' component={ProdutoNav}/>
           <Tab.Screen name='Sobre_a_loja' component={Sobre}/>
           <Tab.Screen name='Cartões' component={Cartoes}/>
-        </Tab.Navigator>
+        </Tab.Navigator>    
       </NavigationContainer>
     )
   }
